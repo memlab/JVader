08c30a3d791a3551791b14ca83f979449516cb43
Yuvi Masory <ymasory@gmail.com>
2010-06-08 18:33:59 -0400

    add arg processing

--- src/edu/upenn/psych/memory/jvader/Main.scala
+++ src/edu/upenn/psych/memory/jvader/Main.scala
@@ -1,8 +1,25 @@
 package edu.upenn.psych.memory.jvader
 
 object Main {
-
+	
+	val usage = "Usage: scala Main file1.wav [file2.wav file3.wav ...]\n\n" +
+				"For each file the filename is printed followed by ordered pairs corresponding to predicted word endpoints."
+				   
 	def main(args: Array[String]) {
-		println("pywr!")
+		if (args.length < 1)	usageAndExit()
+		val files = 
+			for {arg <- args} 
+				yield new java.io.File(arg)
+		for {file <- files} {
+			if (file.exists == false) {
+				Console.err.println(file + " does not exist!")
+				System.exit(1)
+			}
+		}
+	}
+	
+	def usageAndExit() {
+		Console.err.println(usage)
+		System.exit(1)
 	}
 }
