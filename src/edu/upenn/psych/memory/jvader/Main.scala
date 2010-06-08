541fe5d70cb74b63e69f9ef0991845bf7ca93fec
Yuvi Masory <ymasory@gmail.com>
2010-06-08 19:00:16 -0400

    improve functional style

--- src/edu/upenn/psych/memory/jvader/Main.scala
+++ src/edu/upenn/psych/memory/jvader/Main.scala
@@ -1,24 +1,28 @@
 package edu.upenn.psych.memory.jvader
 
+import java.io.File
+
+/**
+ * Entry point of application.
+ * 
+ * @author Yuvi Masory
+ */
 object Main {
 	
-	val usage = "Usage: scala Main file1.wav [file2.wav file3.wav ...]\n\n" +
-				"For each file the filename is printed followed by ordered pairs corresponding to predicted word endpoints."
+	private val usage = "Usage: scala Main file1.wav [file2.wav file3.wav ...]\n\n" +
+						"For each file the filename is printed followed by ordered pairs corresponding to predicted word endpoints."
 				   
 	def main(args: Array[String]) {
-		if (args.length < 1) usageAndExit()
+		if (args.length < 1) 
+			usageAndExit()
+		
 		val files = 
-			for {arg <- args} 
-				yield new java.io.File(arg)
-		for {file <- files} {
-			if (file.exists == false) {
-				Console.err.println(file + " does not exist!")
-				System.exit(1)
-			}
-		}
+			for {arg <- args
+				file = new File(arg)
+				if file.exists} yield file
 	}
 	
-	def usageAndExit() {
+	private def usageAndExit() {
 		Console.err.println(usage)
 		System.exit(1)
 	}
